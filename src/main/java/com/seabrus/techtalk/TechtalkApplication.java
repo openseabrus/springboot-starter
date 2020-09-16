ed5b1f610a527b9546a8cc83ba7eae368fc39d27
seabrgui <guilherme.seabra@diconium.com>
2020-09-16 10:11:17 +0100

    initial

--- src/main/java/com/seabrus/techtalk/TechtalkApplication.java
+++ src/main/java/com/seabrus/techtalk/TechtalkApplication.java
@@ -1,7 +1,5 @@
 package com.seabrus.techtalk;
 
-import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,14 +17,7 @@ public class TechtalkApplication {
   public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
     return args -> {
 
-      System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-      String[] beanNames = ctx.getBeanDefinitionNames();
-      Arrays.sort(beanNames);
-      for (String beanName : beanNames) {
-        System.out.println(beanName);
-      }
-
+      System.out.println("Started Tech Talk's SpringBoot.");
     };
   }
 
